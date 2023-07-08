d7ad6eb06f624ad424081bf28075d26c56637ba2
WoodenBrick1 <brickwooden@gmail.com>
2023-07-08 16:53:20 +0200

    im dumb

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
 
 //Get a random computer output
-function getComputerOutput() {
+
+function getComputerOutput()
+ {
 const random  = Math.random();
 
 
 return (random < 0.33) ? "Rock" : (random > 0.66) ? "Paper" : "Scissors";
 }
 
+//check to see if the player lost or won
 
-function playRound(computerSelection , userSelection) {
+function playRound(computerSelection , userSelection)
+ {
 
+  const result = computerSelection + userSelection;
 
+  if (result == "Rockscissors" || result == "Scissorspaper" || result == "Paperrock")
+   {
+    ++computerScore;
+    return `You Lose! You picked ${userSelection} ,but the computer picked ${computerSelection}`;
+   }
+   else if (result == "Rockrock" || result == "Scissorsscissors" || result == "Paperpaper") 
+   {
+    return `You Tied! You picked ${userSelection} ,and the computer picked ${computerSelection}`;
+   }
+   else if ((result == "Rockpaper" || result == "Scissorsrock" || result == "Paperscissors") )
+    {
+    ++userScore;
+    return `You Win! You picked ${userSelection} ,and the computer picked ${computerSelection}`;
+   }
 }
 
-//Get user input and make it lower case
-const userSelection = prompt("What's your move?").toLowerCase();
+function game()
+ {
+    for(let i = 1; i <= 5; ++i)
+    {
+    //Get user input and make it lower case
+    const userSelection = prompt("What's your move?").toLowerCase();
 const computerSelection = getComputerOutput();
-console.log(computerSelection);
+    console.log(playRound(computerSelection, userSelection));
+    console.log(userScore + "-" + computerScore);
+    }
+  
+   return (computerScore < userScore) ? "YOU WIN :)" : (userScore < computerScore) ? "YOU LOSE :(" : "YOU TIED :/";
+ }
 
 
-/* 
+ let userScore = 0;
+ let computerScore = 0;
+console.log(game());
 
 
 
 
 
 
-check to see if the player lost or won
-display the appropiate message
-
-*/
